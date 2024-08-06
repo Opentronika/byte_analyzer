baeb78b3799d4d71d51182a3b90da308fa3a0ea9
LutGaru <lutgaru@gmail.com>
2024-08-05 20:02:53 -0700

    main structure

---
+++ src/components/ValueElement.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <input v-model=hexNormalizedSize type="text" class=" w-60 border-e-gray-300 border rounded-md p-2">
+  </div>
+</template>
+
+<script>
+import { ref,computed } from 'vue';
+
+export default {
+    name: 'ValueElement',
+    emits: ["selectCharacter"],
+    data(){
+        return{
+        }
+    },
+    props: {
+        value: Number
+    },
+    methods: {
+    },
+    setup(props){
+        const normalizedSize = ref(props.value)
+        const hexNormalizedSize = computed({
+      get() {
+        return `0x${normalizedSize.value.toString(16).padStart(2, '0').toUpperCase()}`;
+      },
+      set(value) {
+        const number = parseInt(value, 16);
+        if (!isNaN(number)) {
+          normalizedSize.value = number;
+        }
+      }
+    });
+        return{
+            normalizedSize,
+            hexNormalizedSize
+        };
+    }   
+}
+</script>
+
+<style>
+</style>
