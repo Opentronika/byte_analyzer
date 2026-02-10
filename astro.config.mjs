24a6cd62e9c862c234e8624cd1579216c7b274e0
lutgaru <lutgaru@gmail.com>
2026-02-09 20:44:42 -0800

    feat: migrate to Tailwind CSS v4 and update Astro config
    - Upgrade `tailwindcss` to v4.1.18
    - Add `@tailwindcss/vite` and configure it in `astro.config.mjs`
    - Update project dependencies (Vue, PrimeIcons, Robots.txt)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import robotsTxt from "astro-robots-txt";
-import tailwind from "@astrojs/tailwind";
-
+import tailwindcss from "@tailwindcss/vite";
 import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://opentronika.github.io',
   base: 'byte_analyzer',
-  integrations: [tailwind(), vue(),robotsTxt()],
+   vite: {
+        plugins: [tailwindcss()],
+      },
+  integrations: [vue(),robotsTxt()],
 });
